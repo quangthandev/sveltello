0e4c54a28d745a89a78b5b4c6f72250f0cff29c7
Quang Than <quangthan87@gmail.com>
2024-01-24 15:12:21 +0700

    Setup basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
 	import '../app.css';
 </script>
 
+<div
+	class="bg-slate-900 border-b border-slate-800 flex items-center justify-between py-4 px-8 box-border"
+>
+	<a href="/" class="font-black text-white text-2xl"> Home </a>
+	<div class="flex items-center gap-6">links</div>
+	<div class="w-1/3 flex justify-end">
+		<a href="/login" class="text-slate-500 text-xs uppercase font-bold">Login</a>
+	</div>
+</div>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1 class="text-red-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-full flex flex-col items-center pt-20 bg-slate-900">
+	<div class="space-y-4 max-w-md text-lg text-slate-300">
+		<p>
+			It's a recreation of the popular drag and drop interface in{' '}
+			<a href="https://trello.com" class="underline"> Trello </a>{' '}
+			and other similar apps.
+		</p>
+		<p>If you want to play around, click sign up!</p>
+	</div>
+	<div class="flex w-full justify-evenly max-w-md mt-8 rounded-3xl p-10 bg-slate-800">
+		<a href="/signup" class="text-xl font-medium text-teal-400 underline"> Sign up </a>
+		<div class="h-full border-r border-slate-500" />
+		<a href="/login" class="text-xl font-medium text-teal-400 underline"> Login </a>
+	</div>
+</div>
