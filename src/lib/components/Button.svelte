214836adc54fb3f861725932feb3a26d274889d0
Quang Than <quangthan87@gmail.com>
2024-01-26 16:25:16 +0700

    Tweak

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -7,6 +7,8 @@
 <button
 	class="flex w-full justify-center rounded-md bg-blue-400 px-1 py-1 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
 	{...$$restProps}
+	on:click
+	on:keydown
 >
 	<slot />
 </button>
