3a1205354527c6971812dd83f5020e795c5f03d2
Quang Than <quangthan87@gmail.com>
2024-03-11 17:11:53 +0700

    Use rich text editor for item description

--- src/lib/actions/click-outside.ts
+++ src/lib/actions/click-outside.ts
@@ -1,27 +1,102 @@
-import type { ActionReturn } from 'svelte/action';
+// Reference: https://github.com/melt-ui/melt-ui/blob/develop/src/lib/internal/actions/click-outside/action.ts
 
-interface ClickOutsideAttibutes {
-	'on:clickOutside': (e: CustomEvent<PointerEvent>) => void;
-}
+import { readable, type Readable } from 'svelte/store';
+import { get } from 'svelte/store';
+import { addEventListener, isFunction } from '$lib/utils.js';
 
-export function clickOutside(node: HTMLElement): ActionReturn<unknown, ClickOutsideAttibutes> {
-	const handleClick = (event: Event) => {
-		if (node && !node.contains(event.target as Node) && !event.defaultPrevented) {
-			node.dispatchEvent(
-				new CustomEvent('clickOutside', {
-					detail: event
-				})
-			);
+export type ClickOutsideConfig = {
+	/**
+	 * Whether the listener is active.
+	 *
+	 * @defaultValue `true`
+	 */
+	enabled?: boolean | Readable<boolean>;
+
+	/**
+	 * Callback when user clicks outside a given element.
+	 */
+	handler?: (evt: PointerEvent) => void;
+
+	/**
+	 * A predicate function or a list of elements that should not trigger the event.
+	 */
+	ignore?: ((e: PointerEvent) => boolean) | Element[];
+};
+
+/**
+ * Creates a readable store that tracks the latest PointerEvent that occurred on the document.
+ *
+ * @returns A function to unsubscribe from the event listener and stop tracking pointer events.
+ */
+const documentClickStore = readable<PointerEvent | undefined>(undefined, (set): (() => void) => {
+	/**
+	 * Event handler for pointerdown events on the document.
+	 * Updates the store's value with the latest PointerEvent and then resets it to undefined.
+	 */
+	function clicked(event: PointerEvent | undefined) {
+		set(event);
+
+		// New subscriptions will not trigger immediately
+		set(undefined);
+	}
+
+	// Adds a pointerdown event listener to the document, calling the clicked function when triggered.
+	const unsubscribe = addEventListener(document, 'pointerup', clicked, {
+		passive: false,
+		capture: true
+	});
+
+	// Returns a function to unsubscribe from the event listener and stop tracking pointer events.
+	return unsubscribe;
+});
+
+export const clickOutside = (node: HTMLElement, config: ClickOutsideConfig = {}) => {
+	let options = { enabled: true, ...config };
+
+	// Returns true if the click outside handler is enabled
+	function isEnabled(): boolean {
+		return typeof options.enabled === 'boolean' ? options.enabled : get(options.enabled);
+	}
+
+	// Handle document clicks
+	const unsubscribe = documentClickStore.subscribe((e) => {
+		// If the click outside handler is disabled, or if the event is null or the node itself, return early
+		if (!isEnabled() || !e || e.target === node) {
+			return;
 		}
-	};
 
-	document.addEventListener('click', handleClick, true);
-	document.addEventListener('touchstart', handleClick, true);
+		const composedPath = e.composedPath();
+
+		// If the target is in the node, return early
+		if (composedPath.includes(node)) return;
+
+		// If an ignore function is passed, check if it returns true
+		if (options.ignore) {
+			if (isFunction(options.ignore)) {
+				if (options.ignore(e)) return;
+			}
+			// If an ignore array is passed, check if any elements in the array match the target
+			else if (Array.isArray(options.ignore)) {
+				if (
+					options.ignore.length > 0 &&
+					options.ignore.some((ignoreEl) => {
+						return ignoreEl && (e.target === ignoreEl || composedPath.includes(ignoreEl));
+					})
+				)
+					return;
+			}
+		}
+
+		// If none of the above conditions are met, call the handler
+		options.handler?.(e);
+	});
 
 	return {
+		update(params: Partial<ClickOutsideConfig>) {
+			options = { ...options, ...params };
+		},
 		destroy() {
-			document.removeEventListener('click', handleClick, true);
-			document.removeEventListener('touchstart', handleClick, true);
+			unsubscribe();
 		}
 	};
-}
+};
--- src/routes/items/[id]/ItemDecription.svelte
+++ src/routes/items/[id]/ItemDecription.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { clickOutside } from '$lib/actions/click-outside';
 	import { useQueryClient } from '@tanstack/svelte-query';
-	import { tick } from 'svelte';
+	import { clickOutside } from '$lib/actions/click-outside';
+	import { escapeKeydown } from '$lib/actions/escape-keydown';
+	import TextEditor from '$lib/components/TextEditor.svelte';
 
 	export let id: string;
 	export let boardId: string;
 	export let content: string | null;
 
 	let isEditing = false;
-	let contentEl: HTMLTextAreaElement;
+	let textEditor: TextEditor;
+	let isSubmitting = false;
 
 	const queryClient = useQueryClient();
 </script>
 
-<div class="flex items-start gap-x-2 mb-8 w-full">
+<div class="relative flex items-start gap-x-2 mb-8 w-full">
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		width="24"
@@ -34,14 +36,20 @@
 	<div class="px-2 w-full">
 		<h3 class="text-xl font-medium mb-4">Description</h3>
 		{#if isEditing}
-			<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 			<form
 				action="?/updateItemContent"
 				method="POST"
-				use:enhance={() => {
+				use:enhance={({ formData }) => {
+					isSubmitting = true;
+
+					const editor = textEditor.getEditor();
+					const content = editor.getSemanticHTML() || '';
+					formData.append('content', content);
+
 					return async ({ update }) => {
 						await update({ invalidateAll: false });
 
+						isSubmitting = false;
 						isEditing = false;
 
 						queryClient.invalidateQueries({
@@ -52,45 +60,67 @@
 						});
 					};
 				}}
-				on:keydown={(e) => {
-					if (e.key === 'Escape') {
+				use:escapeKeydown={{
+					handler: () => (isEditing = false)
+				}}
+				use:clickOutside={{
+					handler: (e) => {
+						e.preventDefault();
 						e.stopPropagation();
+
+						if (textEditor.hasFocus()) return;
 
 						isEditing = false;
 					}
 				}}
-				use:clickOutside
-				on:clickOutside={() => (isEditing = false)}
 			>
-				<textarea
-					bind:this={contentEl}
-					name="content"
-					class="border border-slate-400 w-full rounded-lg py-1 px-2 font-medium"
-					value={content}
-					rows="6"
+				<TextEditor
+					class="bg-white min-h-48"
+					options={{
+						placeholder: 'Add a more detailed description...'
+					}}
+					initialContent={content}
+					autofocus={true}
+					bind:this={textEditor}
 				/>
 				<input type="hidden" name="id" value={id} />
-				<div class="flex items-center gap-x-2">
-					<button type="submit" class="bg-blue-600 text-white rounded-lg py-2 px-4 font-medium">
+				<div class="flex items-center gap-x-2 mt-2">
+					<button
+						type="submit"
+						class="bg-blue-600 text-white rounded-md py-2 px-4 font-medium disabled:bg-neutral-100 disabled:text-neutral-300 disabled:cursor-not-allowed"
+						disabled={isSubmitting}
+					>
 						Save
 					</button>
-					<button on:click={() => (isEditing = false)}>Cancel</button>
+					<button
+						on:click={() => (isEditing = false)}
+						class="px-4 py-2 font-medium rounded-md hover:bg-gray-300">Cancel</button
+					>
 				</div>
 			</form>
 		{:else}
 			<p class="relative min-h-[60px] font-medium rounded-md">
-				<button
-					class="absolute inset-0 text-left px-4 bg-gray-200 hover:bg-gray-300"
-					aria-label="Edit description"
-					on:click|preventDefault={async () => {
-						isEditing = true;
-						await tick();
-						contentEl.focus();
-					}}
-				>
-					{content || 'Add a more detailed description...'}
-				</button>
+				{#if content !== null && content.trim() !== ''}
+					{@html content}
+				{:else}
+					<button
+						class="absolute inset-0 text-left px-4 bg-gray-200 hover:bg-gray-300"
+						aria-label="Edit description"
+						on:click|preventDefault={() => (isEditing = true)}
+					>
+						{'Add a more detailed description...'}
+					</button>
+				{/if}
 			</p>
+		{/if}
+
+		{#if content !== null && content.trim() !== '' && !isEditing}
+			<button
+				class="absolute top-0 right-4 px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300"
+				on:click={() => (isEditing = true)}
+			>
+				Edit
+			</button>
 		{/if}
 	</div>
 </div>
