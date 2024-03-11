fd3fc7de34af1b9fef2181043c074b82abeddc8f
Quang Than <quangthan87@gmail.com>
2024-03-11 17:10:08 +0700

    Add TextEditor component using quill

---
+++ src/lib/components/TextEditor.svelte
@@ -0,0 +1,143 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import type { ActionReturn } from 'svelte/action';
+	import type Quill from 'quill';
+	import type { Delta } from 'quill/core';
+	import type { EmitterSource } from 'quill/core/emitter';
+	import type { Range } from 'quill/core/selection';
+
+	interface StringMap {
+		[key: string]: any;
+	}
+
+	interface QuillOptionsStatic {
+		debug?: boolean;
+		modules?: StringMap | undefined;
+		placeholder?: string | undefined;
+		readOnly?: boolean | undefined;
+		theme?: string | undefined;
+		formats?: string[] | undefined;
+		bounds?: HTMLElement | string | undefined;
+		scrollingContainer?: HTMLElement | string | undefined;
+		strict?: boolean | undefined;
+	}
+
+	export let initialContent: string | null = '';
+	export let options: QuillOptionsStatic = {};
+	export let autofocus: boolean = false;
+
+	let quill: Quill;
+	let editor: HTMLDivElement;
+
+	const dispatch = createEventDispatcher<{
+		initialized: Quill;
+		'text-change': { delta: Delta; oldContent: Delta; source: EmitterSource };
+		'selection-change': { range: Range; oldRange: Range; source: EmitterSource };
+	}>();
+
+	const defaultOptions: QuillOptionsStatic = {
+		modules: {
+			toolbar: [
+				['bold', 'italic'],
+				['link', 'blockquote', 'code-block', 'image'],
+				[{ list: 'ordered' }, { list: 'bullet' }]
+			]
+		},
+		theme: 'snow'
+	};
+
+	type TextEditorOptions = QuillOptionsStatic;
+
+	interface TextEditorAttributes {}
+
+	function quillEditor(
+		node: HTMLDivElement,
+		options?: QuillOptionsStatic
+	): ActionReturn<TextEditorOptions, TextEditorAttributes> {
+		import('quill').then(({ default: Quill }) => {
+			quill = new Quill(node, {
+				...defaultOptions,
+				...options
+			});
+
+			dispatch('initialized', quill);
+
+			if (initialContent) {
+				if (typeof initialContent === 'string') {
+					quill.setContents(
+						quill.clipboard.convert({
+							html: initialContent,
+							text: initialContent
+						})
+					);
+				} else {
+					quill.setContents(initialContent);
+				}
+			}
+
+			if (autofocus) {
+				quill.focus();
+
+				// Move cursor to the end
+				const length = quill.getLength();
+				quill.setSelection(length, length);
+			}
+
+			// Set up event listeners
+			quill.on('text-change', handleTextChange);
+			quill.on('selection-change', handleSelectionChange);
+		});
+
+		function handleTextChange(delta: Delta, oldContent: Delta, source: EmitterSource) {
+			dispatch('text-change', { delta, oldContent, source });
+		}
+
+		function handleSelectionChange(range: Range, oldRange: Range, source: EmitterSource) {
+			dispatch('selection-change', { range, oldRange, source });
+		}
+
+		return {
+			update(newOptions) {
+				options = newOptions;
+			},
+			destroy() {
+				// Remove event listeners
+				quill.off('text-change', handleTextChange);
+				quill.off('selection-change', handleSelectionChange);
+			}
+		};
+	}
+
+	export function getContents() {
+		return quill.getContents();
+	}
+
+	export function getEditor() {
+		if (!quill) {
+			throw new Error('Quill editor is not initialized');
+		}
+
+		return quill;
+	}
+
+	export function focus() {
+		if (!quill) return;
+		quill.focus();
+	}
+
+	export function hasFocus() {
+		if (!quill) return false;
+		return quill.hasFocus();
+	}
+
+	export function blur() {
+		if (!quill) return;
+		quill.blur();
+	}
+</script>
+
+<div bind:this={editor} use:quillEditor={options} {...$$restProps} />
+
+<style>
+	@import url('https://cdn.jsdelivr.net/npm/quill@2.0.0-rc.2/dist/quill.snow.css');
+</style>
