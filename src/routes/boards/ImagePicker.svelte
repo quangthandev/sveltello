949c3847cdbddd45bf5d21463a4e52bf03908e03
Quang Than <quangthan87@gmail.com>
2024-03-19 16:11:11 +0700

    Allow users to reload new random images

--- src/routes/boards/ImagePicker.svelte
+++ src/routes/boards/ImagePicker.svelte
@@ -20,7 +20,27 @@
 </script>
 
 <div>
-	<h6 class="text-sm font-medium leading-6 text-gray-900 mb-1">Background</h6>
+	<div class="flex items-center justify-between">
+		<h6 class="text-sm font-medium leading-6 text-gray-900">Background</h6>
+		<button
+			type="button"
+			class="text-muted-foreground p-2 rounded-md hover:bg-gray-300"
+			aria-label="reload"
+			on:click={() => $query.refetch()}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				width="24"
+				height="24"
+				fill="currentColor"
+			>
+				<path
+					d="M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z"
+				/>
+			</svg>
+		</button>
+	</div>
 	{#if $query.isLoading || $query.isFetching || !$query.data}
 		<Skeleton class={cn('w-full h-20 bg-neutral-200')} />
 	{:else if $query.data}
