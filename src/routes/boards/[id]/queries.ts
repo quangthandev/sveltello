bc9a779edc44bbe328f56a5c57625757cd7f8561
Quang Than <quangthan87@gmail.com>
2024-04-10 14:03:58 +0700

    Use drizzle for queries

--- src/routes/boards/[id]/queries.ts
+++ src/routes/boards/[id]/queries.ts
@@ -1,20 +1,20 @@
-import prisma from '$lib/server/prisma';
-import type { ItemMutation } from './types';
+import { db } from '$lib/server/drizzle/db';
+import { and, asc, eq } from 'drizzle-orm';
+import { attachment, board, column, cover, item } from '$lib/server/drizzle/schema';
+import { getColumnByUserBoard, getItemByUserBoard } from '$lib/server/drizzle/utils';
+import type { Attachment, Item, ItemMutation } from '../../types';
 
 export async function getBoard(boardId: number, userId: string) {
-	return prisma.board.findUnique({
-		where: {
-			id: boardId,
-			userId
-		},
-		include: {
+	return await db.query.board.findFirst({
+		where: and(eq(board.id, boardId), eq(board.userId, userId)),
+		with: {
 			items: true,
 			columns: {
-				orderBy: { order: 'asc' },
-				include: {
+				orderBy: [asc(column.order)],
+				with: {
 					items: {
-						orderBy: { order: 'asc' },
-						include: {
+						orderBy: [asc(column.order)],
+						with: {
 							cover: true,
 							attachments: true
 						}
@@ -26,115 +26,195 @@ export async function getBoard(boardId: number, userId: string) {
 }
 
 export async function updateBoardName(boardId: number, name: string, userId: string) {
-	return prisma.board.update({
-		where: { id: boardId, userId },
-		data: { name }
-	});
+	return await db
+		.update(board)
+		.set({ name })
+		.where(and(eq(board.id, boardId), eq(board.userId, userId)));
 }
 
 export async function createColumn(boardId: number, name: string, userId: string, id?: string) {
-	const columnCount = await prisma.column.count({
-		where: { boardId, board: { userId } }
-	});
-
-	return prisma.column.create({
-		data: {
-			id: id ?? crypto.randomUUID(),
-			boardId,
-			name,
-			order: columnCount + 1
+	const existed = await db.query.board.findFirst({
+		where: and(eq(board.id, boardId), eq(board.userId, userId)),
+		with: {
+			columns: {
+				orderBy: [asc(column.order)]
+			}
 		}
 	});
+
+	if (!existed) {
+		throw new Error('Board not found');
+	}
+
+	return await db
+		.insert(column)
+		.values({
+			id,
+			boardId,
+			name,
+			order: existed.columns.length + 1
+		})
+		.returning();
 }
 
 export async function updateColumnName(id: string, name: string, userId: string) {
-	return prisma.column.update({
-		where: { id, board: { userId } },
-		data: { name }
-	});
+	const existed = await db
+		.select()
+		.from(board)
+		.leftJoin(column, eq(column.boardId, board.id))
+		.where(and(eq(column.id, id), eq(board.userId, userId)))
+		.get();
+
+	if (!existed?.Board) {
+		throw new Error('Board not found');
+	}
+
+	return await db
+		.update(column)
+		.set({ name })
+		.where(and(eq(column.id, id), eq(column.boardId, existed.Board.id)));
 }
 
-export async function copyColumn(id: string, name: string, userId: string) {
-	const columnToCopy = await prisma.column.findUnique({
-		where: { id, board: { userId } },
-		include: {
-			items: {
-				orderBy: { order: 'asc' }
-			}
+export async function copyColumn(id: string, name: string, boardId: number, userId: string) {
+	const existed = await getColumnByUserBoard(id, userId);
+
+	if (!existed?.Board) {
+		throw new Error('Column not found');
+	}
+
+	const columnItems = await db.query.item.findMany({
+		where: and(eq(item.columnId, id), eq(item.boardId, existed.Board.id)),
+		orderBy: [asc(item.order)],
+		with: {
+			attachments: true
 		}
 	});
 
-	if (!columnToCopy) {
+	if (columnItems.length === 0) {
 		return;
 	}
 
-	const boardColumns = await prisma.column.findMany({
-		where: { boardId: columnToCopy.boardId }
+	const boardColumns = await db.query.column.findMany({
+		where: eq(column.boardId, existed.Board.id),
+		orderBy: [asc(column.order)]
 	});
 
 	let newOrder: number;
-	const nextColumn = boardColumns.find((column) => column.order > columnToCopy.order);
+	const nextColumn = boardColumns.find((column) => column.order > existed.Column.order);
 	if (!nextColumn) {
-		newOrder = columnToCopy.order + 1;
+		newOrder = existed.Column.order + 1;
 	} else {
-		newOrder = (columnToCopy.order + nextColumn.order) / 2;
+		newOrder = (existed.Column.order + nextColumn.order) / 2;
 	}
 
-	return await prisma.column.create({
-		data: {
-			id: crypto.randomUUID(),
-			boardId: columnToCopy.boardId,
+	// Create new column
+	const createdColumn = await db
+		.insert(column)
+		.values({
+			boardId,
 			name,
-			order: newOrder,
-			items: {
-				create: columnToCopy.items.map((item) => ({
-					id: crypto.randomUUID(),
+			order: newOrder
+		})
+		.returning()
+		.get();
+
+	// Copy items
+	const createdItems = await db
+		.insert(item)
+		.values(
+			columnItems
+				.map((item) => ({
+					boardId: createdColumn.boardId,
+					columnId: createdColumn.id,
 					title: item.title,
 					content: item.content,
-					order: item.order,
-					boardId: columnToCopy.boardId
+					order: item.order
 				}))
-			}
-		},
-		include: {
-			items: true
+				.filter((item): item is Item => item !== null)
+		)
+		.returning();
+
+	// Copy attachments and cover
+	for (const item of columnItems) {
+		if (!item || item.attachments.length === 0) {
+			continue;
 		}
-	});
+
+		const itemCover = await db.query.cover.findFirst({
+			where: eq(cover.itemId, item.id)
+		});
+
+		const attachmentsToCopy = item.attachments
+			.map((attachment) => {
+				const createdItemId = createdItems.find((i) => i.title === item.title)?.id;
+
+				if (!createdItemId) {
+					return null;
+				}
+
+				return {
+					itemId: createdItemId,
+					name: attachment.name,
+					url: attachment.url,
+					type: attachment.type
+				};
+			})
+			.filter((attachment): attachment is Attachment => attachment !== null);
+
+		await db.insert(attachment).values(attachmentsToCopy);
+
+		if (itemCover) {
+			await db.insert(cover).values({
+				itemId: createdItems.find((i) => i.title === item.title)!.id,
+				url: itemCover.url
+			});
+		}
+	}
 }
 
 export async function deleteColumn(id: string, userId: string) {
-	return prisma.column.delete({
-		where: { id, board: { userId } }
-	});
+	const existed = await getColumnByUserBoard(id, userId);
+
+	if (!existed?.Board) {
+		throw new Error('Column not found');
+	}
+
+	return await db
+		.delete(column)
+		.where(and(eq(column.id, existed.Column.id), eq(column.boardId, existed.Board.id)));
 }
 
 export async function updateColumnOrder(id: string, order: number, userId: string) {
-	return prisma.column.update({
-		where: { id, board: { userId } },
-		data: { order }
-	});
+	const existed = await getColumnByUserBoard(id, userId);
+
+	if (!existed?.Board) {
+		throw new Error('Column not found');
+	}
+
+	return await db.update(column).set({ order }).where(eq(column.id, existed.Column.id));
 }
 
-export function upsertItem(mutation: ItemMutation & { boardId: number }, userId: string) {
-	return prisma.item.upsert({
-		where: {
-			id: mutation.id,
-			board: {
-				userId
-			}
-		},
-		create: mutation,
-		update: mutation
-	});
+export async function upsertItem(mutation: ItemMutation, userId: string) {
+	const existed = await getItemByUserBoard(mutation.id, userId);
+
+	// Create if not existed
+	if (!existed?.Item) {
+		return await db.insert(item).values(mutation).returning();
+	}
+
+	// Update
+	return await db.update(item).set(mutation).where(eq(item.id, existed.Item.id)).returning();
 }
 
-export function moveItemToColumn(id: string, columnId: string, userId: string) {
-	return prisma.item.update({
-		where: { id, board: { userId } },
-		data: { columnId }
-	});
-}
+export async function moveItemToColumn(id: string, columnId: string, userId: string) {
+	const existed = await getItemByUserBoard(id, userId);
 
-export function deleteCard(id: string, userId: string) {
-	return prisma.item.delete({ where: { id, board: { userId } } });
+	if (!existed?.Item) {
+		throw new Error('Item not found');
+	}
+
+	return db
+		.update(item)
+		.set({ columnId })
+		.where(and(eq(item.id, existed.Item.id), eq(item.boardId, existed.Item.boardId)));
 }
