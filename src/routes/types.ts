2e982ebe9b5154ecae159a6ab979ae14e5584320
Quang Than <quangthan87@gmail.com>
2024-04-10 14:01:38 +0700

    Change types based on drizzle schemas

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,31 +1,32 @@
-import type { Prisma } from '@prisma/client';
+import type { attachment, board, column, cover, item } from '$lib/server/drizzle/schema';
 
-export type BoardWithColumns = Prisma.BoardGetPayload<{
-	include: {
-		columns: {
-			include: {
-				items: true;
-			};
-		};
-	};
-}>;
+export type Board = typeof board.$inferSelect;
+export type Column = typeof column.$inferSelect;
+export type Item = typeof item.$inferSelect;
+export type Attachment = typeof attachment.$inferSelect;
+export type Cover = typeof cover.$inferSelect;
 
-export type ItemWithColumn = Prisma.ItemGetPayload<{
-	include: {
-		column: true;
-		cover: true;
-	};
-}>;
+export type BoardWithColumns = Board & {
+	columns: (Column & { items: ItemWithCoverAndAttachments[] })[];
+};
 
-export type ItemWithCoverAndAttachments = Prisma.ItemGetPayload<{
-	include: {
-		cover: true;
-		attachments: true;
-	};
-}>;
+export type ItemWithColumn = Item & {
+	column: Column;
+};
+export type ItemFullPayload = Item & {
+	board: Board;
+	column: Column;
+	cover: Cover | null;
+	attachments: (Attachment & { isCover: boolean })[];
+};
+export type ItemWithCoverAndAttachments = Item & {
+	cover: Cover | null;
+	attachments: ItemFullPayload['attachments'];
+};
+export type ItemMutation = Required<
+	Omit<typeof item.$inferInsert, 'content' | 'createdAt' | 'updatedAt'>
+>;
 
-export type ColumnWithItems = Prisma.ColumnGetPayload<{
-	include: {
-		items: true;
-	};
-}>;
+export type ColumnWithItems = Column & {
+	items: Item[];
+};
