1c27860668cf387c41d30165ed7649cc5bf3b2fb
Quang Than <quangthan87@gmail.com>
2024-04-26 15:10:14 +0700

    Fix filename case issue

---
+++ src/lib/components/ui/skeleton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	let className: string;
+	export { className as class };
+</script>
+
+<div class={cn('animate-pulse rounded-md bg-gray-200', className)} {...$$props} />
---
+++ src/lib/features/columns/components/list.svelte
@@ -0,0 +1,168 @@
+<script lang="ts">
+	import ListItem from './list-item.svelte';
+	import ListActionsMenu from './list-actions-menu.svelte';
+	import { tick } from 'svelte';
+	import { dndzone, type DndEvent, TRIGGERS, SHADOW_PLACEHOLDER_ITEM_ID } from 'svelte-dnd-action';
+	import IconPlus from '$lib/components/icons/icon-plus.svelte';
+	import type { ItemWithCoverAndAttachments } from '$lib/types';
+	import EditableText from '$lib/components/shared/editable-text.svelte';
+	import NewItem from '$lib/features/items/components/new-item.svelte';
+	import { useUpdateItem } from '$lib/features/items/query-client/mutations';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	export let name: string;
+	export let boardName: string;
+	export let boardId: number;
+	export let columnId: string;
+	export let items: ItemWithCoverAndAttachments[];
+
+	let localItems: ItemWithCoverAndAttachments[] = items;
+
+	$: if (items) {
+		dealWithServerUpdate();
+	}
+	function dealWithServerUpdate() {
+		const shadowItem = localItems.find((item) => item.id === SHADOW_PLACEHOLDER_ITEM_ID);
+
+		if (!shadowItem) {
+			localItems = items;
+		} else {
+			localItems = items.map((sItem) =>
+				localItems.find((cItem) => sItem.id === cItem.id) ? sItem : shadowItem
+			);
+		}
+	}
+
+	let editing: boolean = false;
+	let listEl: HTMLOListElement;
+
+	let sourceIndex: number | null = null;
+
+	function scrollList() {
+		if (listEl) {
+			listEl.scrollTop = listEl.scrollHeight;
+		}
+	}
+
+	const updateItemMutation = useUpdateItem(boardId);
+
+	function handleDndConsider(cId: string, e: CustomEvent<DndEvent<ItemWithCoverAndAttachments>>) {
+		if (columnId === cId) {
+			// Store source index when user starts dragging for later use
+			if (e.detail.info.trigger === TRIGGERS.DRAG_STARTED) {
+				sourceIndex = items.findIndex((item) => item.id === e.detail.info.id);
+			}
+
+			localItems = e.detail.items;
+		}
+	}
+
+	function handleDndFinalize(cId: string, e: CustomEvent<DndEvent<ItemWithCoverAndAttachments>>) {
+		if (columnId === cId) {
+			const { items: newItems } = e.detail;
+
+			localItems = newItems;
+
+			if (e.detail.info.trigger === TRIGGERS.DROPPED_INTO_ZONE) {
+				// Get the index of the item that was dropped
+				const droppedItemId = e.detail.info.id;
+				const droppedIndex = newItems.findIndex((i) => i.id === droppedItemId);
+
+				// If the item was dropped in the same position and same column, do nothing
+				if (sourceIndex === droppedIndex && columnId === newItems[droppedIndex].columnId) return;
+
+				// Calculate the new order for the dropped item
+				const previousOrder = newItems[droppedIndex - 1] ? newItems[droppedIndex - 1].order : 0;
+				const nextOrder = newItems[droppedIndex + 1]
+					? newItems[droppedIndex + 1].order
+					: previousOrder + 1;
+				const newOrder = (previousOrder + nextOrder) / 2;
+
+				// Update the item
+				const item = localItems.find((item) => item.id === droppedItemId);
+				if (item) {
+					$updateItemMutation.mutate({ ...item, order: newOrder, columnId });
+				}
+
+				// Reset the source index
+				sourceIndex = null;
+			}
+		}
+	}
+
+	function transformDraggedElement(draggedEl?: HTMLElement) {
+		if (!draggedEl) return;
+
+		const listItem = draggedEl.querySelector('.list-item');
+
+		if (listItem) {
+			(listItem as HTMLElement).style.transform = 'rotate(8deg)';
+		}
+	}
+</script>
+
+<li
+	class="flex-shrink-0 flex flex-col gap-1 overflow-hidden max-h-full w-80 border-slate-400 rounded-xl shadow-sm shadow-slate-400 bg-neutral-100"
+>
+	<div class="p-2 flex justify-between items-center">
+		<EditableText
+			action="?/updateColumnName"
+			fieldName="name"
+			value={name}
+			class="w-full"
+			inputClassName="w-full"
+			buttonClassName="block text-left w-full"
+		>
+			<input type="hidden" name="id" value={columnId} />
+		</EditableText>
+		<ListActionsMenu id={columnId} {name} onAddCard={() => (editing = true)} />
+	</div>
+
+	<ol
+		bind:this={listEl}
+		class="flex-grow overflow-auto min-h-4"
+		use:dndzone={{
+			items: localItems,
+			flipDurationMs: 300,
+			type: 'items',
+			transformDraggedElement
+		}}
+		on:consider={(e) => handleDndConsider(columnId, e)}
+		on:finalize={(e) => handleDndFinalize(columnId, e)}
+	>
+		{#each localItems as item (item.id)}
+			<li>
+				<ListItem {item} {boardName} class="list-item" />
+			</li>
+		{/each}
+	</ol>
+
+	{#if editing}
+		<NewItem
+			{boardId}
+			{columnId}
+			nextOrder={localItems.length === 0 ? 1 : localItems[localItems.length - 1].order + 1}
+			on:create={async () => {
+				await tick();
+				scrollList();
+			}}
+			on:complete={() => (editing = false)}
+		/>
+	{:else}
+		<div class="p-2">
+			<Button
+				variant="ghost"
+				on:click={async () => {
+					editing = true;
+					await tick();
+					scrollList();
+				}}
+				aria-label="Add a card"
+				class="flex justify-start items-center gap-2 w-full text-muted-foreground"
+			>
+				<IconPlus />
+				Add a card
+			</Button>
+		</div>
+	{/if}
+</li>
