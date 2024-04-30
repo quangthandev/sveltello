fe387c01b5aa785dccd5ba429c532c962d3f30e8
Quang Than <quangthan87@gmail.com>
2024-04-30 16:07:09 +0700

    Avoid hydration mismatch

--- src/routes/(user)/+layout.svelte
+++ src/routes/(user)/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import SidebarLeft from '$lib/components/sidebars/sidebar-left.svelte';
 	import { sidebarPreference } from '$lib/stores/sidebar-preference';
 	import { cn } from '$lib/utils';
@@ -6,19 +7,21 @@
 	$: expanded = $sidebarPreference.expanded;
 </script>
 
-<div class="flex flex-grow h-full">
-	<SidebarLeft
-		{expanded}
-		on:toggle={(event) => {
-			sidebarPreference.set({ expanded: event.detail });
-		}}
-	/>
-	<main
-		class={cn('w-full h-full', {
-			'pl-[var(--app-sidebar-width)]': expanded,
-			'pl-4': !expanded
-		})}
-	>
-		<slot />
-	</main>
-</div>
+{#if browser}
+	<div class="flex flex-grow h-full">
+		<SidebarLeft
+			{expanded}
+			on:toggle={(event) => {
+				sidebarPreference.set({ expanded: event.detail });
+			}}
+		/>
+		<main
+			class={cn('w-full h-full', {
+				'pl-[var(--app-sidebar-width)]': expanded,
+				'pl-4': !expanded
+			})}
+		>
+			<slot />
+		</main>
+	</div>
+{/if}
