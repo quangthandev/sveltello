c77772b85a985c3f9410836f1216718f4623c538
Quang Than <quangthandev@gmail.com>
2024-05-10 20:32:40 +0700

    Add Sentry config

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,19 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
 import { lucia } from '$lib/server/auth';
 import { redirect } from '@sveltejs/kit';
+import { PUBLIC_SENTRY_DSN } from '$env/static/public';
+
+Sentry.init({
+	dsn: PUBLIC_SENTRY_DSN,
+	tracesSampleRate: 1
+});
 
 const PUBLIC_ROUTES = ['/', '/login', '/signup'];
 
-export async function handle({ event, resolve }) {
+export const handleError = Sentry.handleErrorWithSentry();
+
+export const handle = sequence(Sentry.sentryHandle(), async function _handle({ event, resolve }) {
 	const sessionId = event.cookies.get(lucia.sessionCookieName);
 
 	if (!sessionId && !PUBLIC_ROUTES.includes(event.url.pathname)) {
@@ -35,4 +45,4 @@ export async function handle({ event, resolve }) {
 	}
 
 	return await resolve(event);
-}
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { sentrySvelteKit } from '@sentry/sveltekit';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: process.env.SENTRY_ORG,
+				project: process.env.SENTRY_PROJECT
+			}
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
