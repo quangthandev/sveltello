774857e30fcd84380ff6a0c29cc63fbde1226211
Quang Than <quangthandev@gmail.com>
2024-05-10 13:37:56 +0700

    Fix unhandled response error from client queries

--- src/lib/features/boards/query-client/queries.ts
+++ src/lib/features/boards/query-client/queries.ts
@@ -1,24 +1,44 @@
 import { createQuery } from '@tanstack/svelte-query';
 import type { Board, BoardWithColumns } from '$lib/types';
 
+const BASE_URL = '/boards';
+
 export const useBoards = (initialData: Board[] = []) =>
 	createQuery<BoardWithColumns[]>({
 		queryKey: ['boards'],
-		queryFn: async () => (await fetch('/boards')).json(),
+		queryFn: async () => {
+			const res = await fetch(BASE_URL);
+
+			if (!res.ok) {
+				throw new Error('Failed to fetch boards');
+			}
+
+			return await res.json();
+		},
 		initialData: initialData.map((board) => ({ ...board, columns: [] }))
 	});
 
 export const useBoard = (id: number, initialData?: BoardWithColumns) => {
+	const queryFn = async () => {
+		const res = await fetch(`${BASE_URL}/${id}`);
+
+		if (!res.ok) {
+			throw new Error('Failed to fetch board');
+		}
+
+		return await res.json();
+	};
+
 	if (!initialData) {
 		return createQuery<BoardWithColumns>({
 			queryKey: ['boards', id],
-			queryFn: async () => (await fetch(`/boards/${id}`)).json()
+			queryFn
 		});
 	}
 
 	return createQuery<BoardWithColumns>({
 		queryKey: ['boards', id],
-		queryFn: async () => (await fetch(`/boards/${id}`)).json(),
+		queryFn,
 		initialData
 	});
 };
--- src/lib/features/items/query-client/queries.ts
+++ src/lib/features/items/query-client/queries.ts
@@ -2,22 +2,26 @@ import { createQuery } from '@tanstack/svelte-query';
 import type { ItemFullPayload } from '$lib/types';
 
 export const useItem = (id: string, initialData?: ItemFullPayload) => {
+	const queryFn = async () => {
+		const res = await fetch(`/items/${id}`);
+
+		if (!res.ok) {
+			throw new Error('Failed to fetch item');
+		}
+
+		return await res.json();
+	};
+
 	if (!initialData) {
 		return createQuery<ItemFullPayload>({
 			queryKey: ['items', id],
-			queryFn: async () => {
-				const res = await fetch(`/items/${id}`);
-				return res.json();
-			}
+			queryFn
 		});
 	}
 
 	return createQuery<ItemFullPayload>({
 		queryKey: ['items', id],
-		queryFn: async () => {
-			const res = await fetch(`/items/${id}`);
-			return res.json();
-		},
+		queryFn,
 		initialData
 	});
 };
